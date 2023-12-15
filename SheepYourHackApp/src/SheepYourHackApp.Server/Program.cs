37ec17b32910c90476978b8cca89810956bc9709
lukaszsawina <lukisaw01@gmail.com>
2023-12-15 18:41:27 +0100

    Api controller fix with automapper and initial swagger startup

--- SheepYourHackApp/src/SheepYourHackApp.Server/Program.cs
+++ SheepYourHackApp/src/SheepYourHackApp.Server/Program.cs
@@ -1,12 +1,13 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using NextjsStaticHosting.AspNetCore;
+using System;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
-// Step 1: Add Next.js hosting support
 builder.Services.Configure<SheepYourHackHostingOptions>(builder.Configuration.GetSection("NextjsStaticHosting"));
 builder.Services.AddNextjsStaticHosting();
 builder.Services.AddSwaggerGen();

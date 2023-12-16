ca90d0a7b128ef8d101bf4d4b79476546e8cd3d3
Arkadiusz Zając <arekzaj7@gmail.com>
2023-12-16 05:42:42 +0100

    Feeds

--- SheepYourHackApp/src/SheepYourHackApp.Server/Controllers/FeedsController.cs
+++ SheepYourHackApp/src/SheepYourHackApp.Server/Controllers/FeedsController.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 using SheepYourHackApp.Server.Handlers;
 using SheepYourHackApp.Server.Models.DTO;
 using System.Collections.Generic;
@@ -27,6 +28,14 @@ namespace SheepYourHackApp.Server.Controllers
             var result = await _mediator.Send(new GetAllFeedQueruRequest());
 
             return Ok(_mapper.Map<List<FeedDto>>(result));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] FeedDto feedDto)
+        {
+            await _mediator.Send(new CreateFeedCommandRequest(feedDto));
+
+            return CreatedAtRoute("Get", new { id = feedDto.Id }, feedDto);
         }
     }
 }
---
+++ SheepYourHackApp/src/SheepYourHackApp.Server/Handlers/CreateFeedCommandHandler.cs
@@ -0,0 +1,44 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using SheepYourHackApp.Server.Models;
+using SheepYourHackApp.Server.Models.DTO;
+using SheepYourHackApp.Server.UnitsOfWork;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SheepYourHackApp.Server.Handlers
+{
+    public class CreateFeedCommandRequest : IRequest<Feed>
+    {
+        public FeedDto feedDto { get; }
+
+        public CreateFeedCommandRequest(FeedDto feedDto)
+        {
+            this.feedDto = feedDto;
+        }
+
+    }
+
+    public class CreateFeedCommandHandler : IRequestHandler<CreateFeedCommandRequest, Feed>
+    {
+
+        private readonly IMapper _mapper;
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly ILogger<CreateFeedCommandRequest> _logger;
+
+        public CreateFeedCommandHandler(IUnitOfWork unitOfWork, ILogger<CreateFeedCommandRequest> logger, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _logger = logger;
+            _mapper = mapper;
+        }
+
+        public async Task<Feed> Handle(CreateFeedCommandRequest request, CancellationToken cancellationToken)
+        {
+            var result = _mapper.Map<Feed>(request.feedDto);
+            await _unitOfWork.Feeds.Add(result);
+            return result;
+        }
+    }
+}
